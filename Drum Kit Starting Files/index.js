d85259f4b1eb346f37e29cd737f59d2817ec3bfd
rinij <rinimani7@gmail.com>
2024-08-01 22:51:55 -0600

    used anonymous function in event listener

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,8 +1,17 @@
-document.querySelector("button").addEventListener("click",handleClick)
+// document.querySelector("button").addEventListener("click",handleClick)
 
-function handleClick(){
+// function handleClick(){
+//     alert("I got clicked!")
+// }
+
+//using anonymus function
+
+document.querySelector("button").addEventListener("click",function(){
     alert("I got clicked!")
-}
+    })
+
+
+
 
 //while adding the event listeners, the function is called without the paranthesis.
 //If the function is called with the paranthesis, it becomes a method, and get called when the event listener is added and not when the button is clicked (ie the event)
