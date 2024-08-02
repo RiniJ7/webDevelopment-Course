e604825e1437027d5302f8f96df91cbb0272475c
rinij <rinimani7@gmail.com>
2024-08-02 16:22:03 -0600

    modifying the color of button to white

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -6,23 +6,19 @@
 
 //using anonymus function
 
-for (let i=0 ; i < document.querySelectorAll(".drum").length ; i++)
-    {
-    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
     //   here a new variable is declared to which a new audio object is assigned
-    //then we use the method called .play() to play the audio   
+    //then we use the method called .play() to play the audio
 
-
-
-        });
-
+    this.style.color = "white";
+  });
 }
 
+//this you can tap the identity of the button that triggered the event
 
 // var audio = new Audio('sounds/tom-1.mp3');
 // audio.play();
-
-
 
 //while adding the event listeners, the function is called without the paranthesis.
 //If the function is called with the paranthesis, it becomes a method, and get called when the event listener is added and not when the button is clicked (ie the event)
