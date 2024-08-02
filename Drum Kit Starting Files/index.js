221a3f590434e8f1888df018c057d2722f6bf6bf
rinij <rinimani7@gmail.com>
2024-08-01 23:02:31 -0600

    event listeners added to all keys with for loop

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -6,9 +6,15 @@
 
 //using anonymus function
 
-document.querySelector("button").addEventListener("click",function(){
-    alert("I got clicked!")
-    })
+for (let i=0 ; i < document.querySelectorAll(".drum").length ; i++)
+    {
+    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+        alert("I got clicked!");
+        });
+
+}
+
+
 
 
 
