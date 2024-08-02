80574aaea8eabcd3afb5c3cebd05a1db3b2137a9
rinij <rinimani7@gmail.com>
2024-08-01 22:41:21 -0600

    details added

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -5,6 +5,6 @@ function handleClick(){
 }
 
 //while adding the event listeners, the function is called without the paranthesis.
-//If the function is called with the paranthesis, it becomes a method, and get called when the event listener is added and not when the button is clicked
+//If the function is called with the paranthesis, it becomes a method, and get called when the event listener is added and not when the button is clicked (ie the event)
 
 // The document.querySelector method selects the first element in the document that matches the specified CSS selector. In your case, the selector "button" targets all <button> elements. However, querySelector will only return the first match it finds in the DOM.
