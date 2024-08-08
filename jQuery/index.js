81a83fdc4a26244bab2e80c2d12090cf5fef1896
rinij <rinimani7@gmail.com>
2024-08-08 11:01:56 -0600

    new jQuery style added to button

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,6 +1,14 @@
 alert("Working");
 
 
-$(document).ready(function(){
-    $("h1").css("color", "red");
-})
+// $(document).ready(function(){
+//     $("h1").css("color", "red");
+// })
+
+
+//the below is the example of setting the value
+$("h1").css("color", "red");
+//the below is the example of getting the value
+console.log($("h1").css("color"));
+console.log($("h1").css("font-size"));
+$("button").css("color", "purple");
