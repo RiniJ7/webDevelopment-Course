f1a176b9c0c3f03f278802fa071c000544e1bd2a
rinij <rinimani7@gmail.com>
2024-08-08 11:20:25 -0600

    adding multiple classes to jQuery

--- jQuery/index.js
+++ jQuery/index.js
@@ -7,8 +7,20 @@ alert("Working");
 
 
 //the below is the example of setting the value
-$("h1").css("color", "red");
+//$("h1").css("color", "red");
 //the below is the example of getting the value
 console.log($("h1").css("color"));
 console.log($("h1").css("font-size"));
 $("button").css("color", "purple");
+
+//adding class to selected element
+$("h1").addClass("big-title");
+
+
+//remove class to selected element
+$("h1").removeClass("big-title");
+
+//adding multiple classes to jQuery
+
+$("h1").addClass("big-title margin-50");
+
