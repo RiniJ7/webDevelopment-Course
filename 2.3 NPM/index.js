1552a976243a7958b1761ce82e65cfa0ae4831d0
rinij <rinimani7@gmail.com>
2024-09-09 17:18:56 -0600

    randomSuperHero generator function created

--- 2.3 NPM/index.js
+++ 2.3 NPM/index.js
@@ -1,5 +1,13 @@
 //var generateName = require('sillyname');
 
 import generateName from "sillyName";
+import {randomSuperhero} from "superheroes";
+
+
+
 var sillyName = generateName();
+let superHeroName = randomSuperhero();
+
 console.log(`My name is ${sillyName}.`)
+
+console.log(`I am a superhero. My name is ${superHeroName}`)
