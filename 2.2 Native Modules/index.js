ab9065cfde97d0cbe8e55467e25cdd9f88cd0d29
rinij <rinimani7@gmail.com>
2024-09-09 16:14:52 -0600

    using readFile and utf8 to read from a file

--- 2.2 Native Modules/index.js
+++ 2.2 Native Modules/index.js
@@ -1,6 +1,13 @@
 const fs = require("fs");
 
-fs.writeFile("message.txt", "hello from NodeJS", (err) => {
-    if (err) throw (err);
-    console.log("FIle has been saved");
+// fs.writeFile("message.txt", "hello from NodeJS", (err) => {
+//     if (err) throw (err);
+//     console.log("FIle has been saved");
+// });
+
+
+
+fs.readFile("./message.txt","utf8", (err,data) => {
+    if (err) throw err;
+    console.log(data);
 });
