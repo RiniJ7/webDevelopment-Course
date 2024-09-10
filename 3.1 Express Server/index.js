0f681304903539bc54bd726d8f0839006f3503b3
rinij <rinimani7@gmail.com>
2024-09-10 11:35:11 -0600

    get method for server side rendering

--- 3.1 Express Server/index.js
+++ 3.1 Express Server/index.js
@@ -2,6 +2,9 @@ import express from "express";
 const app = express();
 //the port number of the server is defined here
 const port = 3000;
+app.get("/",(req,res) => {
+  res.send("Hello World !");
+});
 
 //the .listen method is applied on app constant. It consist of a port number and a call back function
 app.listen(port, () => {
