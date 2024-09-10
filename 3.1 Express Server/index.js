fbc304c22b22a75afca4b3b0d3de6004ba066561
rinij <rinimani7@gmail.com>
2024-09-10 11:06:53 -0600

    commentation

--- 3.1 Express Server/index.js
+++ 3.1 Express Server/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 const app = express();
+//the port number of the server is defined here
 const port = 3000;
 
+//the .listen method is applied on app constant. It consist of a port number and a call back function
 app.listen(port, () => {
   console.log(`Server running on port ${port}.`);
 });
