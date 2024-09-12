d299645a8cebbcca2b100b41f81e894f4d3e047e
rinij <rinimani7@gmail.com>
2024-09-12 14:38:41 -0600

    using body parser , logger morgan

--- 3.4 Middleware/index2.js
+++ 3.4 Middleware/index2.js
@@ -1,7 +1,17 @@
 import express from "express";
+import morgan from "morgan";
 
 const app = express();
 const port = 3000;
+
+//app.use(morgan("tiny"));
+
+app.use((re,res,next) => {
+  console.log("Request method: ", req.method);
+  next();
+});
+
+
 
 app.get("/", (req, res) => {
   res.send("Hello");
