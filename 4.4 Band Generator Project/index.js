565e4fc0a380a3d99a6e4f5d81f60492d5e1fb35
rinij <rinimani7@gmail.com>
2024-10-17 12:17:29 -0600

    logic to generate random names using math.floor method

--- 4.4 Band Generator Project/index.js
+++ 4.4 Band Generator Project/index.js
@@ -4,8 +4,11 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
+
 //Step 3 - Make the styling show up.
+
 //Hint 1: CSS files are static files!
+app.use(express.static("public"));
 //Hint 2: The header and footer are partials.
 //Hint 3: Add the CSS link in header.ejs
 
@@ -15,10 +18,19 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
+  res.render("index.ejs");
   //Step 1 - Make the get route work and render the index.ejs file.
 });
 
 app.post("/submit", (req, res) => {
+
+  const randomAdj = adj[Math.floor(Math.random() * adj.length)];
+  const randomNoun = noun[Math.floor(Math.random() * noun.length)];
+
+  res.render("index.ejs", {
+    adjective: randomAdj,
+    noun: randomNoun,
+  });
   //Step 2 - Make the generate name functionality work
   //Hint: When the "Generate Name" button in index.ejs is clicked, it should hit up this route.
   //Then:
